6d221a21b675d894240d89ce019cb2423a5ad2b4
Eduardo Rossel <eduardorossel@outlook.cl>
2020-12-03 18:54:29 -0300

    added profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -50,6 +50,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
+  def show
+    @user = User.find_by(id: current_user.id)
+  end
+
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
   #   super(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,10 @@ Rails.application.routes.draw do
 
   get 'home/admin_panel', 'home/admin_panel'
   get 'home/make_admin', 'home/make_admin' 
-  get 'home/remove_admin', 'home/remove_admin' 
+  get 'home/remove_admin', 'home/remove_admin'
 
+  devise_scope :user do 
+    get 'users/registrations/show' => 'users/registrations/show'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
